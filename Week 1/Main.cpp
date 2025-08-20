7e0cb394a251e113a936d958df33bc64fb9b8467
Chrome0X <araiasaraujo@gmail.com>
2025-08-20 13:47:31 -0700

    Create Main.cpp

---
+++ Week 1/Main.cpp
@@ -0,0 +1,10 @@
+//HW 1
+#include <iostream> 
+using namespace std; 
+int main() 
+{
+    cout << "Hello Inventory System!\n";
+    cout << "Int size:\t" << sizeof(int) << "\n";
+    cout << "Float size:\t" << sizeof(float) << "\n";
+    cout << "Char size:\t" << sizeof(char);
+}
