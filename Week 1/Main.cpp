97af105a5bf8ec5e641eea4b62efda8fd85facc4
Chrome0X <araiasaraujo@gmail.com>
2025-08-20 21:43:26 -0700

    after lab

--- Week 1/Main.cpp
+++ Week 1/Main.cpp
@@ -1,10 +1,9 @@
 //HW 1
 #include <iostream> 
 using namespace std; 
-int main() 
-{
+int main() {
     cout << "Hello Inventory System!\n";
-    cout << "Int size:\t" << sizeof(int) << "bytes\n";
-    cout << "Float size:\t" << sizeof(float) << "bytes\n";
-    cout << "Char size:\t" << sizeof(char) << "bytes";
+    cout << "Int size:\t" << sizeof(int) << " bytes\n";
+    cout << "Float size:\t" << sizeof(float) << " bytes\n";
+    cout << "Char size:\t" << sizeof(char) << " bytes";
 }
